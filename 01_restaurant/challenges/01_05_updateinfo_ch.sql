2f899e5c12f4cd071888da5634ff5a0c9ccb0250
Emily Duran <emilynbauer@outlook.com>
2023-06-19 10:17:33 -0500

    Finished challenge 4 & 5

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,25 @@
 
 -- Taylor Jenkins, of 27170 6th Ave., Washington, DC,
 -- has moved to 74 Pine St., New York, NY.
+
+SELECT
+  FirstName,
+  LastName, 
+  Address,
+  City,
+  State
+
+FROM Customers
+
+WHERE
+  FirstName = 'Taylor'
+  AND
+  LastName = 'Jenkins';
+
+Update Customers
+SET
+  Address = '74 Pine St.',
+  City = 'New York',
+  State = 'NY'
+WHERE 
+  CustomerID = 26;
