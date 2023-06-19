22b5e92ea0501f92f96cbe98d547b0a1f40cb9fc
Emily Duran <emilynbauer@outlook.com>
2023-06-19 09:54:39 -0500

    Finished Challenge 3

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,24 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+SELECT Type, Name, Price, Description
+FROM 
+  dishes
+ORDER BY price DESC;
+
+SELECT *
+FROM 
+  Dishes
+WHERE
+  type = "Appetizer"
+  OR
+  type = "Beverage"
+ORDER BY type;
+
+SELECT *
+FROM 
+  Dishes
+WHERE
+  type <> "Beverage"
+ORDER BY type;
