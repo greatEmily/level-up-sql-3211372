7b27b739a34c03abcab205dfefb42f19a9e32b7c
Emily Duran <emilynbauer@outlook.com>
2023-06-20 11:56:01 -0500

    Finished challenge 6

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,27 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT *
+
+FROM Reservations r
+JOIN Customers c ON r.CustomerID = c.CustomerID
+
+WHERE
+FirstName = 'Norby'
+AND
+Date > '2022-07-24';
+
+SELECT
+CustomerID, 
+ReservationID, 
+Date
+
+FROM
+Reservations
+
+WHERE
+CustomerID = 64;
+
+DELETE FROM Reservations
+WHERE ReservationID = 2000;
